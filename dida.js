ebc34ef6e3d63f0964ba712630ad57222e7fd3c1
WayneDuan <duany0816@gmail.com>
2022-05-19 18:09:25 +0800

    Update dida.js

--- dida.js
+++ dida.js
@@ -16,11 +16,10 @@ hostname = dida365.com
 var body = $response.body;
 var objc = JSON.parse(body);
 var timestamp = (new Date()).valueOf();
-objc = 
-      {
+objc = {
         "userId": "1013918876",
   "username": "0lusmpfe@user.dida365.com",
-  "proEndDate": "2999-09-28",
+  "proEndDate": "2999-09-28T00:00:00.000+0000",
   "needSubscribe": false,
   "inboxId": "inbox1013918876",
   "teamUser": false,
