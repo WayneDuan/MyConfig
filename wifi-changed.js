a42d6a4ee4040e183b5dd0e6eca2567345b99123
WayneDuan <duany0816@gmail.com>
2022-07-29 18:13:10 +0800

    Update wifi-changed.js

--- wifi-changed.js
+++ wifi-changed.js
@@ -4,9 +4,4 @@ const mode = WIFI_PROXYS.includes($network.wifi.ssid)
     ? 'global-proxy'
     : 'rule';
   $surge.setOutboundMode(mode);
-  $notification.post(
-    'Surge',
-    `Wi-Fi changed to ${$network.wifi.ssid || 'cellular'}`,
-    `use ${mode} mode`
-  );
 $done();
