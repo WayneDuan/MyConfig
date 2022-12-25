ed6154b4abe4fc6f554eff9e70c67a38b630d34d
WayneDuan <duany0816@gmail.com>
2022-12-25 10:23:26 +0800

    Update 91.js

--- 91.js
+++ 91.js
@@ -1,6 +1,6 @@
 
 var hausd0rff = $request.headers;
 
-  hausd0rff['X-AUTH-TOKEN'] = 'eyJhbGciOiJIUzUxMiIsImlhdCI6MTY3MDg0MTYwMSwiZXhwIjoxNjg2MzkzNjAxfQ.eyJpZCI6MTEyNDM1MTY0fQ.cDXTx14EaNf1TJzihOxOoIB4lyfwKnNNSrXbDiBj43kXzkzkqfftQRAqE23WTCuxQ45VYB-1IadrwxG_t3QoNA';
+  hausd0rff['x-auth-token'] = 'eyJhbGciOiJIUzUxMiIsImlhdCI6MTY3MDg0MTYwMSwiZXhwIjoxNjg2MzkzNjAxfQ.eyJpZCI6MTEyNDM1MTY0fQ.cDXTx14EaNf1TJzihOxOoIB4lyfwKnNNSrXbDiBj43kXzkzkqfftQRAqE23WTCuxQ45VYB-1IadrwxG_t3QoNA';
 
 $done({headers : hausd0rff});
