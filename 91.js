fe581080dd3fac6a4ec5766b0339260956f89a25
WayneDuan <duany0816@gmail.com>
2022-12-26 20:25:06 +0800

    Update 91.js

--- 91.js
+++ 91.js
@@ -1,6 +1,6 @@
 
 var hausd0rff = $request.headers;
-var data=JSON.stringify(hausd0rff).toLowerCase()
+var data=JSON.parse(JSON.stringify(hausd0rff).toLowerCase());
 data['x-auth-token'] = 'eyJhbGciOiJIUzUxMiIsImlhdCI6MTY3MDg0MTYwMSwiZXhwIjoxNjg2MzkzNjAxfQ.eyJpZCI6MTEyNDM1MTY0fQ.cDXTx14EaNf1TJzihOxOoIB4lyfwKnNNSrXbDiBj43kXzkzkqfftQRAqE23WTCuxQ45VYB-1IadrwxG_t3QoNA';
-console.log(data)
+console.log(data);
 $done({headers : data});
