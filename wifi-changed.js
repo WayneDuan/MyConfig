9e633ba1d0eaae10697163811da2f0e3814bae27
WayneDuan <duany0816@gmail.com>
2023-01-09 19:53:38 +0800

    Update wifi-changed.js

--- wifi-changed.js
+++ wifi-changed.js
@@ -2,9 +2,11 @@ const cellular_PROXYS = ['460-11'];
 const network = JSON.parse(JSON.stringify($network))
 const mcc = network['cellular-data']['carrier']
 const wifi = $network.wifi.ssid
-
-const mode = wifi == '' && cellular_PROXYS.includes(mcc)
+console.log(wifi)
+const mode = wifi == null && cellular_PROXYS.includes(mcc)
     ? 'global-proxy'
     : 'rule';
+
+console.log(mode)
 $surge.setOutboundMode(mode);
 $done();
