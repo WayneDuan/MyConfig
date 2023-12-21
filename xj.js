35eb33007224dd7d05bb8100554acc4c805cc732
WayneDuan <duany0816@gmail.com>
2023-12-21 20:03:16 +0800

    Update xj.js

--- xj.js
+++ xj.js
@@ -8,8 +8,6 @@ QuantumultX:
 hostname = *.megdnvg.cn
 ***************************
 */
-
-
 var body = $response.body;
 var objc = JSON.parse(body);
 var timestamp = (new Date()).valueOf();
@@ -48,19 +46,8 @@ objc = {
             "isvip": 1,
             "gender": 0
         },
-        "signed": 0
+        "signed": 1
     }
-    "userId": "1013918876",
-    "username": "0lusmpfe@user.dida365.com",
-    "proEndDate": "2999-09-28T00:00:00.000+0000",
-    "needSubscribe": false,
-    "inboxId": "inbox1013918876",
-    "teamUser": false,
-    "activeTeamUser": false,
-    "freeTrial": true,
-    "timeStamp": timestamp,
-    "pro": true,
-    "ds": true
 };
 
 body = JSON.stringify(objc);
