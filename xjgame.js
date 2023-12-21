76c4a07bc6d912cc3bde16423c79ec582d91ce87
WayneDuan <duany0816@gmail.com>
2023-12-21 20:25:01 +0800

    Update xjgame.js

--- xjgame.js
+++ xjgame.js
@@ -1,1 +1,18 @@
+var body = $response.body;
+var objc = JSON.parse(body);
+var timestamp = (new Date()).valueOf();
+objc = {
+    "retcode" : 0,
+  "errmsg" : "",
+  "data" : {
+    "data" : [
+    ]
+  }
 
+};
+
+body = JSON.stringify(objc);
+
+$done({
+    body
+});
