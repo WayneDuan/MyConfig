a87d8a7bb58675821de50f936c54219b67a3dadd
WayneDuan <duany0816@gmail.com>
2023-12-21 21:03:36 +0800

    更新 xjad.js

--- xjad.js
+++ xjad.js
@@ -1,1 +1,11 @@
-sssd
+var body = $response.body;
+var objc = JSON.parse(body);
+var timestamp = (new Date()).valueOf();
+objc.data.pcsliderows={};
+objc.data.v2sliderows={};
+
+body = JSON.stringify(objc);
+
+$done({
+    body
+});
